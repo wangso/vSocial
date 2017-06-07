28c5a134ce345bea59e2e56af406b824cbe62660
Adam Starr <Adam@The-Revs-MacBook-Pro.local>
2017-06-07 11:38:00 -0500

    formated code and made not one big comment

---
+++ itemSpawner.js
@@ -0,0 +1,55 @@
+//
+// itemSpawner.js 
+//
+// Created by Clement Brisset on 11/16/2016. 
+// Copyright 2015 High Fidelity, Inc. //
+// This script spawns items for the ping pong table 
+//
+// Distributed under the Apache License, Version 2.0. 
+// See the accompanying file LICENSE or http:
+// www.apache.org/licenses/LICENSE-2.0.html 
+//
+ (function () 
+ 	{
+ 	 var ITEM_LIFETIME = 2300;
+  //
+ 30 min. var itemID;
+  this.preload = function(entityID) 
+  {
+   itemID = entityID;
+  
+  }
+   this.spawnItem = function () 
+  {
+   var properties = Entities.getEntityProperties(itemID, [ "type", "name", "modelURL", "shapeType", "parentID", "position", "rotation", "dimensions", "gravity", "restitution", "collisionSoundURL", ]);
+  var parent = Entities.getEntityProperties(properties.parentID, ["position", "rotation"]);
+  var pos = 
+  {
+   x: 0.0, y: 0.50, z: properties.name.indexOf("blue") !== -1 ? 1.40 : -1.40, 
+}
+;
+  pos = Vec3.sum(parent.position, Vec3.multiplyQbyV(parent.rotation, pos));
+  print("Spawning a new " + properties.name);
+  Entities.addEntity(
+  	{
+  	 type: properties.type, name: "Spawned " + properties.name, modelURL: properties.modelURL, shapeType: properties.shapeType, position: pos, rotation: properties.rotation, dimensions: properties.dimensions, velocity: 
+  {
+   x: 0, y: 0.1, z: 0 
+}
+, gravity: properties.gravity, restitution: properties.restitution, lifetime: ITEM_LIFETIME, collisionsWillMove: 1, dynamic: 1, density: 100, collisionSoundURL: properties.collisionSoundURL, 
+}
+);
+  
+  }
+   this.startNearTrigger = function () 
+  {
+   this.spawnItem();
+  
+  }
+   this.startFarTrigger = function () 
+  {
+   this.spawnItem();
+  
+  }
+}
+);
