70dd2b4a11d2bea1a3b5ff851d8d0071fefbc36f
Adam Starr <Adam@mwc-016157.dhcp.missouri.edu>
2017-06-07 16:05:48 -0500

    Add script to create paddle spawner

---
+++ createPaddleSpawner.js
@@ -0,0 +1,9 @@
+Entities.addEntity({
+	position: Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation)),
+	"script": Script.resolvePath("spawnPaddle.js") ,
+	type: "paddle",
+	color: { red: 255, green: 255, blue: 255 },
+	dimensions: { x: .5, y: .1, z: .1 },
+	name: "Paddle Spawner",
+	"userData": "{ \"grabbableKey\": { \"wantsTrigger\": true }, \"wearable\": { \"joints\": { \"RightHand\": [ { \"x\": 0.0813, \"y\": 0.0452, \"z\": 0.0095 }, { \"x\": -0.3946, \"y\": -0.6604, \"z\": 0.4748, \"w\": -0.4275 } ], \"LeftHand\": [ { \"x\": -0.0881, \"y\": 0.0259, \"z\": 0.0159 }, { \"x\": 0.4427, \"y\": -0.6519, \"z\": 0.4592, \"w\": 0.4099 } ] } } }"
+});
