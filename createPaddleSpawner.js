a3db00c138e0ac6486aecf72f7959f53ab4d984c
Adam Starr <acstarr14@gmail.com>
2017-06-08 14:13:38 -0500

    Made type capitalized

--- createPaddleSpawner.js
+++ createPaddleSpawner.js
@@ -1,7 +1,7 @@
 Entities.addEntity({
 	position: Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.orientation)),
 	script: "https://raw.githubusercontent.com/acstarr/vSocial/master/SpawnPaddle.js",
-	type: "paddle",
+	type: "Paddle",
 	name: "Paddle Spawner",
 	"userData": "{ \"grabbableKey\": { \"wantsTrigger\": true }, \"wearable\": { \"joints\": { \"RightHand\": [ { \"x\": 0.0813, \"y\": 0.0452, \"z\": 0.0095 }, { \"x\": -0.3946, \"y\": -0.6604, \"z\": 0.4748, \"w\": -0.4275 } ], \"LeftHand\": [ { \"x\": -0.0881, \"y\": 0.0259, \"z\": 0.0159 }, { \"x\": 0.4427, \"y\": -0.6519, \"z\": 0.4592, \"w\": 0.4099 } ] } } }"
 });
